304d42f7647b49b8e8994456b79454f9d4d40361
KhurshidShod <khurshid.lenovo@gmail.com>
2024-09-20 04:42:56 -0700

    main

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
+  build: {
+    analyze: true,
+  },
   devtools: { enabled: true },
-  css: [
-    '~/assets/css/index.css'
-  ]
-})
+  css: ["~/assets/css/index.css"],
+});
